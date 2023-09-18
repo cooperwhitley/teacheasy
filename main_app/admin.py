97f8cd7ceb529817cdb4e036c1d6d7b8fda19c41
cooperwhitley <whitleycooperr@gmail.com>
2023-09-18 09:48:47 -0500

    update admin registration for all models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import DaysOfTheWeek, Class
+from .models import DaysOfTheWeek, Course, Post, Comment, Assignment, Submission
 
 # Register your models here.
 admin.site.register(DaysOfTheWeek)
-admin.site.register(Class)
+admin.site.register(Course)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Assignment)
+admin.site.register(Submission)
