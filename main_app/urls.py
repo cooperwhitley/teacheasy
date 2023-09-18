c3f21f7f9e3cff3b924cdb2a4a134828eb42c0ff
cooperwhitley <whitleycooperr@gmail.com>
2023-09-18 09:59:33 -0500

    add course urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,13 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Route for home page
+    # Home url
     path('', views.home, name='home'),
-    # Route for about page
+    # About url
     path('about/', views.about, name='about'),
-    # Route for class index
-    # Route for class details
-    # Route for create class
-    # Route for update class
-    # Route for delete class
+    # Course urls
+    path('courses/', views.CourseList.as_view(), name='courses_index'),
+    path('courses/<int:course_id>/', views.CourseDetail.as_view(), name='courses_detail'),
+    path('courses/create/', views.CourseCreate.as_view(), name='courses_create'),
+    path('courses/<int:pk>/update', views.CourseUpdate.as_view(), name='courses_update'),
+    path('courses/<int:pk>/delete', views.CourseDelete.as_view(), name='course_delete'),
+    # Post Views
+    # Comment Views
+    # Assignment Views
+    # Submission Views
 ]
