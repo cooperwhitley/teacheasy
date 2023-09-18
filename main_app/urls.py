8e107fb26602b04e133df976c3540efd03499219
carlosdos818@yahoo.com <carlosdos818@yahoo.com>
2023-09-18 10:14:59 -0400

    adds detail.html,index.html and updates to the urls.py and views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Route for home page
     path('', views.home, name='home'),
+    # Route for about page
     path('about/', views.about, name='about'),
+    # Route for class index
+    # Route for class details
+    # Route for create class
+    # Route for update class
+    # Route for delete class
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Import the Class model
+from .models import Class
 
+# Dummy Data
+Classes = [
+    {
+        'classname': 'John',
+        'teacher':'Jane Smith',
+        'studentsenrolled':'15',
+
+    }
+]
+
+# Create your views here.
+# Home view
 def home(request):
     return render(request, 'home.html')
-
+# About view
 def about(request):
     return render(request, 'about.html')
+# Index view for courses
+def classes_index(request):
+    classes = Class.objects.all()
+    return render(request, 'classes/index.html', { 'classes': classes })
+# Details for a single class
+# classCreate view
+# updateView for a single class
+# deleteView for a single class
