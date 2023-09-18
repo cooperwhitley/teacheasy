cb654e068a269955580aed6b813912b33369e13a
cooperwhitley <whitleycooperr@gmail.com>
2023-09-18 09:49:16 -0500

    add class based views for Course model

--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,45 @@
-from django.shortcuts import render
-
-# Import the Class model
-from .models import Class
-
-# Dummy Data
-Classes = [
-    {
-        'classname': 'John',
-        'teacher':'Jane Smith',
-        'studentsenrolled':'15',
-
-    }
-]
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from main_app.models import Course, Post, Comment, Assignment, Submission
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+import uuid, boto3, os
 
 # Create your views here.
+
 # Home view
 def home(request):
     return render(request, 'home.html')
+
 # About view
 def about(request):
     return render(request, 'about.html')
-# Index view for courses
-def classes_index(request):
-    classes = Class.objects.all()
-    return render(request, 'classes/index.html', { 'classes': classes })
-# Details for a single class
-# classCreate view
-# updateView for a single class
-# deleteView for a single class
+
+# Course Index
+class CourseList(ListView):
+    model: Course
+    template_name = 'courses/index.html'
+
+# CourseDetail
+class CourseDetail(DetailView):
+    model: Course
+    template_name = 'courses/detail.html'
+
+# CourseCreate
+class CourseCreate(CreateView):
+    model = Course
+    fields = '__all__'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+# Course Update
+class CourseUpdate(UpdateView):
+    model = Course
+    fields = '__all__'
+
+# Course Delete
+class CourseDelete(DeleteView):
+    model = Course
+    success_url = '/courses'
