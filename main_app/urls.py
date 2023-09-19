0efddc1e40da94deaf05e5a2a8e10436c2d96996
cooperwhitley <whitleycooperr@gmail.com>
2023-09-19 10:41:53 -0500

    change format for detail urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,20 +10,20 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     # Course urls
     path('courses/', views.CourseList.as_view(), name='courses_index'),
-    path('courses/<int:pk>/', views.CourseDetail.as_view(), name='courses_detail'),
+    path('courses/<int:course_id>/', views.CourseDetail.as_view(), name='courses_detail'),
     path('courses/create/', views.CourseCreate.as_view(), name='courses_create'),
     path('courses/<int:pk>/update', views.CourseUpdate.as_view(), name='courses_update'),
     path('courses/<int:pk>/delete', views.CourseDelete.as_view(), name='courses_delete'),
     # Post Views
     path('posts/', views.PostList.as_view(), name='posts_index'),
-    path('posts/<int:pk>/', views.PostDetail.as_view(), name='posts_detail'),
+    path('posts/<int:post_id>/', views.PostDetail.as_view(), name='posts_detail'),
     path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
     path('posts/<int:pk>/update', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete', views.PostDelete.as_view(), name='posts_delete'),
     # Comment Views
     # Assignment Views
     path('assignments/', views.AssignmentList.as_view(), name='assignments_index'),
-    path('assignments/<int:pk>/', views.AssignmentDetail.as_view(), name='assignments_detail'),
+    path('assignments/<int:assignment_id>/', views.AssignmentDetail.as_view(), name='assignments_detail'),
     path('assignments/create/', views.AssignmentCreate.as_view(), name='assignments_create'),
     path('assignments/<int:pk>/update', views.AssignmentUpdate.as_view(), name='assignments_update'),
     path('assignments/<int:pk>/delete', views.AssignmentDelete.as_view(), name='assignments_delete'),
