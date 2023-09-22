d61b2fe34173e9a43a7190cf694d79e4f11ce95b
cooperwhitley <whitleycooperr@gmail.com>
2023-09-21 23:34:45 -0500

    add assignment and post forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
-from .models import Comment
+from .models import Comment, Post, Assignment
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body',)
+
+class AssignmentForm(ModelForm):
+    class Meta:
+        model = Assignment
+        fields = ('title', 'body', 'due_date',)
