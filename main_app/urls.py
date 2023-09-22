0ecedd3468cbbbbe58c37221df317d81012d41c3
AfternoonDozer <noahmendoza97@gmail.com>
2023-09-22 01:25:05 -0400

    addcreate assignment and post to course detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
+    path('posts/<int:course_id>/add_post/', views.add_post, name='add_post'),
     # Comment Views
     path('posts/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
     path('posts/<int:post_id>/comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
@@ -33,6 +34,7 @@ urlpatterns = [
     path('assignments/create/', views.AssignmentCreate.as_view(), name='assignments_create'),
     path('assignments/<int:pk>/update/', views.AssignmentUpdate.as_view(), name='assignments_update'),
     path('assignments/<int:pk>/delete/', views.AssignmentDelete.as_view(), name='assignments_delete'),
+    path('assignments/<int:course_id>/add_assignment/', views.add_assignment, name='add_assignment'),
     # Submission Views
     path('assignments/<int:assignment_id>/add_submission/', views.upload_submission, name='upload_submission'),
     path('assignments/<int:assignment_id>/submissions/<int:submission_id>/', views.submission_detail, name='submission_detail'),
